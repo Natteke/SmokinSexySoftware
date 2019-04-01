f8757ea1dfe5bf1209ff929fbfdb504866dd7baf
Andrey Ponomarenko <slaner2008@yandex.ru>
2019-04-01 11:09:43 +0300

    Remove test from repo

--- webpack.resolve.js
+++ webpack.resolve.js
@@ -1,17 +1,20 @@
-module.exports = config => {
-	const pefix = config.production ? '' : '.test';
-	const name = config.packageName;
-	return {
-		entry: {
-			[name]: `./packages/${name}/src/index.js`,
-		},
-		output: {
-			path: __dirname +`/packages/${name}/dist`,
-			filename: `${name}${pefix}.js`,
-			libraryTarget: 'umd',
-			libraryExport: 'default',
-			umdNamedDefine: true,
-			library: '[name]',
-		}
-	}
+const path = require('path');
+
+module.exports = (config) => {
+    const stage = config.production ? 'dist' : 'dev';
+    const pefix = config.production ? '' : '.test';
+    const name = config.packageName;
+    return {
+        entry: {
+            [name]: `./packages/${name}/src/index.js`,
+        },
+        output: {
+            path: path.resolve('packages', name, stage),
+            filename: `${name}${pefix}.js`,
+            libraryTarget: 'umd',
+            libraryExport: 'default',
+            umdNamedDefine: true,
+            library: '[name]',
+        },
+    };
 };
