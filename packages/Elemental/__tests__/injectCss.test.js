98a9941d084d1d2dd31a931d90fc453e30d9252e
Andrey Ponomarenko <slaner2008@yandex.ru>
2019-04-08 09:11:00 +0300

    add test for Elemental injectCss

---
+++ packages/Elemental/__tests__/injectCss.test.js
@@ -0,0 +1,12 @@
+import Elemental from '../src/index';
+
+describe('Inject Css', () => {
+    it('element has style in css object', () => {
+        document.body.innerHTML = '<div></div>';
+        const target = document.querySelector('div');
+        target.style.color = 'red';
+        const newStyle = { color: 'blue' };
+        Elemental.assignCss(target, newStyle);
+        expect(target.style.color).toBe('blue');
+    });
+});
